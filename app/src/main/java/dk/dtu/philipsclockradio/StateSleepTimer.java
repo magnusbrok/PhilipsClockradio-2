df5e5462f1eb3cad893febd8f4fa828e63aa36bb
magnusbrok <43874955+magnusbrok@users.noreply.github.com>
2019-11-04 14:18:13 +0100

    addded idle timer in sleepState

--- app/src/main/java/dk/dtu/philipsclockradio/StateSleepTimer.java
+++ app/src/main/java/dk/dtu/philipsclockradio/StateSleepTimer.java
@@ -1,6 +1,13 @@
 package dk.dtu.philipsclockradio;
 
+import android.os.CountDownTimer;
+
 public class StateSleepTimer extends StateAdapter {
+
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private static final long START_TIME_IN_MILLIS = 5000;
 
     int[] sleepIntervals = new int[]{120, 90, 60, 30, 15};
     int timesClicked = 1;
@@ -8,13 +15,15 @@ public class StateSleepTimer extends StateAdapter {
 
     @Override
     public void onEnterState(ContextClockradio context) {
-
+        startTimer(context);
         context.ui.setDisplayText(sleepIntervals[0]+"");
         context.ui.turnOnLED(3);
     }
 
     @Override
     public void onClick_Sleep(ContextClockradio context) {
+        stopTimer();
+        startTimer(context);
 
         if (timesClicked <= 4) {
 
@@ -32,5 +41,25 @@ public class StateSleepTimer extends StateAdapter {
     @Override
     public void onLongClick_Sleep(ContextClockradio context) {
         context.setState(new StateStandby(context.getTime()));
+    }
+
+    public void startTimer(final ContextClockradio context) {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                context.setState(new StateStandby(context.getTime()));
+            }
+        }.start();
+        mTimerRunning = true;
+    }
+
+    public void stopTimer() {
+        mCountDownTimer.cancel();
     }
 }
